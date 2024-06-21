b2a86aed2059ab73892b2c92010c42cdf7794966
ksjitendra18 <jitendraks1803@gmail.com>
2024-06-21 21:54:30 +0530

    dynamic?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { db } from "@/db";
 import { users } from "@/db/schema";
+import { cookies } from "next/headers";
 import React from "react";
 
 const Home = async () => {
+  const token = cookies().get("token");
   const allUsers = await db.select().from(users);
   return <div>{JSON.stringify(allUsers, null, 2)}</div>;
 };
