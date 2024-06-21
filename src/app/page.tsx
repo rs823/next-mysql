51ed92908f467ffc81651b61080ca630e490e1d1
ksjitendra18 <jitendraks1803@gmail.com>
2024-06-21 22:13:07 +0530

    cache

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 import { db } from "@/db";
 import { users } from "@/db/schema";
+import { unstable_cache } from "next/cache";
 import { cookies } from "next/headers";
 import React from "react";
 
-const Home = async () => {
+export const revalidate = 60;
+export const dynamic = "force-dynamic";
+
+const getAllUsers = async () => {
   const token = cookies().get("token");
   const allUsers = await db.select().from(users);
-  return <div>{JSON.stringify(allUsers, null, 2)}</div>;
+  return allUsers;
+};
+
+const cachedGetAllUsers = unstable_cache(
+  async () => {
+    const allUsers = await db.select().from(users);
+    return allUsers;
+  },
+  ["get-all-users"],
+  {
+    tags: ["get-all-users"],
+    revalidate: 60,
+  }
+);
+
+const Home = async () => {
+  const token = cookies().get("token");
+  const allUsers = getAllUsers();
+  const cachedAllUsers = await cachedGetAllUsers();
+  return (
+    <div>
+      {JSON.stringify(allUsers, null, 2)}
+
+      <h2 className="my-5 font-bold">Cached</h2>
+      {JSON.stringify(cachedAllUsers, null, 2)}
+    </div>
+  );
 };
 
 export default Home;
